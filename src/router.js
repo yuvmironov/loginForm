ffc6560af374a0a97e6bdc717f3e52fa6484d030
Yuri Mironov <y.mironov@smartblocklab.com>
2019-10-04 11:07:09 +0300

    формы

--- src/router.js
+++ src/router.js
@@ -5,19 +5,24 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      component: Home,
+      children: [
+        {
+          path: '/login',
+          name: 'login',
+          component: () => import(/* webpackChunkName: "login" */ './components/Login.vue')
+        },
+        {
+          path: '/registration',
+          name: 'registration',
+          component: () => import(/* webpackChunkName: "registration" */ './components/Registration.vue')
+        }
+      ]
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="Menu">
+      <router-link to='/login'>Логин</router-link>
+      <router-link to='/registration'>Регистрация</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+<style>
+  .Menu {
+    width: 200px;
+    margin: 0 auto;
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 20px
   }
+</style>
+
+<script>
+export default {
+  name: 'home'
 }
 </script>
